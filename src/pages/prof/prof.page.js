5d49e9865d0f881aba3a79ab74f5c38b6b95eb4e
Mihaila Andrei <andyfbgm@gmail.com>
2018-03-20 13:02:53 +0200

    prof page ++

--- src/pages/prof/prof.page.js
+++ src/pages/prof/prof.page.js
@@ -5,6 +5,25 @@
             styleUrl: './src/pages/prof/prof.page.css'
         },
         function () {
-
+            
         });
 })();
+
+this.groups = ['B4','B6'];
+
+function switchVisibleGroup(id){
+    document.getElementById('prof-div').style.display = 'none';
+    document.getElementById('group'+id).style.display = 'block';
+}
+
+function clickGroup(id) {
+
+    var newGroup = document.createElement('div');  //create group table here
+    newGroup.class="group-box";
+    newGroup.id="group"+id;
+    newGroup.innerHTML="<h3>"+id+"</h3>";
+
+    document.getElementById('prof-main').appendChild(newGroup);
+    switchVisibleGroup(id);
+}
+
