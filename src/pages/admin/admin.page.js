9e7f3327566bfea0a5135e013c56a7e24c349ca1
Cristian <cristian.ursu@info.uaic.ro>
2018-03-22 04:44:12 +0200

    admin.page.js

--- src/pages/admin/admin.page.js
+++ src/pages/admin/admin.page.js
@@ -11,10 +11,36 @@
         function () {
             this.$onInit = function () {
                 AuthService.setupVision();
+                
+            
             }
             this.$on('#invite','click',function () {
                 alert('invited');
             }.bind(this));
             
-        });
+
+            this.studenti = [{
+                username: "jack23",
+                grupa:"B6"
+            },
+            {
+                username: "maria",
+                grupa:"B6"
+            },
+            {
+                username: "test2",
+                grupa:"B6"
+            }
+        ]
+
+        this.profesori = [{
+            username: "jack23",
+            grupe : [{g1:"B4",g2:"B6"}]
+        }
+    ]
+       
+    
+    
+    
+    });
 })();
