2da6e8f7e0dcb752829f5b49420c19aa30e6742b
Cristian <cristian.ursu@info.uaic.ro>
2018-06-13 23:09:00 +0300

    student and attendance classes created

---
+++ Api/src/03-core/domain/student.ts
@@ -0,0 +1,25 @@
+import { Entity, Column , OneToMany, JoinTable } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import {AttendanceComments} from './attendance-comments';
+@Entity()
+export class Student extends BaseEntity {
+
+    @Column()
+    private firstName: string;
+
+    @Column()
+    private lastName: string;
+
+    @Column()
+    private group: string;
+
+    @Column()
+    private deleted?: boolean = false;
+
+    @OneToMany((type) => AttendanceComments, (attendanceCommets) => attendanceCommets.id)
+    private attendanceComments: Array<AttendanceComments> = new Array<AttendanceComments>();
+    public markAsDeleted(): void {
+        this.deleted = true;
+    }
+
+}
