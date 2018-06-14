33e3114390b24f0bdf2cfba0d7d44b28d6c3307d
Mihaila Andrei <andyfbgm@gmail.com>
2018-06-14 13:19:10 +0300

    can now bind student accounts to user accounts

--- Api/src/02-persistance/student.repository.ts
+++ Api/src/02-persistance/student.repository.ts
@@ -10,14 +10,21 @@ export class StudentRepository extends BaseRepository<Student> {
     constructor(@Inject(DatabaseContext) context: DatabaseContext) {
         super(context);
     }
-    public async getStudentsWithAttendance(receivedGroup: string):  Promise<Array<{}>> {
+
+    public async getStudentsWithAttendance(receivedGroup: string): Promise<Array<{}>> {
         const dbSet = await this.dbSet();
-       // return dbSet.find();
-       // return dbSet.find({ where: { deleted: false, group: receivedGroup}});
-        return dbSet.find({ relations: ["attendanceComments"], where: { deleted: false, group: receivedGroup}});
+        // return dbSet.find();
+        // return dbSet.find({ where: { deleted: false, group: receivedGroup}});
+        return dbSet.find({ relations: ["attendanceComments"], where: { deleted: false, group: receivedGroup } });
     }
-    public async getStudents(recievedGroup : string): Promise<Array<{}>> {
+
+    public async getStudents(recievedGroup: string): Promise<Array<{}>> {
         const dbSet = await this.dbSet();
-        return dbSet.find({where: {deleted : false, group: recievedGroup}});
+        return dbSet.find({ where: { deleted: false, group: recievedGroup } });
+    }
+
+    public async getStudentByCNP(targetcnp: string): Promise<{}> {
+        const dbSet = await this.dbSet();
+        return dbSet.find({ where: { deleted: false, cnp: targetcnp } });
     }
 }
