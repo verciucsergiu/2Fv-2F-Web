804132add41cb29fb3206626f6ccb35404ff6c6a
Cristian <cristian.ursu@info.uaic.ro>
2018-06-15 12:03:13 +0300

    fixed student front

--- Web/src/pages/student-home/student-home.page.js
+++ Web/src/pages/student-home/student-home.page.js
@@ -17,18 +17,19 @@
             this.attendanceArray = [];
             this.id = 1;
             this.attendances = 0;
+            this.currentStudent
             this.maxAttendances = 0;
             this.studentAttendancies = 0;
             this.cnp = "";
-            this.computeChance = (prezente) => {
-                if (prezente > 2) return "DA";
-                return "NU";
-            }
+            this.chances=[];
+            this.promovare="";
+            this.currentAttendancies=0;
             this.$onInit = () => {
                 StudentService.getStudentDetails((response) => {
                     console.log(response.body);
                     let jsonResponse = response.body;
                     this.studentName = jsonResponse.firstName + ' ' + jsonResponse.lastName;
+                    this.currentStudent=this.studentName;
                     this.group = jsonResponse.group;
                     this.cnp = jsonResponse.cnp;
                     StudentService.getStudentsFromGroup(response.body.group, this.callback, this.lookuperr);
@@ -46,21 +47,33 @@
                             this.attendanceArray.push(attendance);
                             this.studentAttendancies++;
                         }
-                        this.attendances++;
+                        if(attendance.value!=="")
+                             this.attendances++;
                     }
                     if (this.attendances > this.maxAttendances) this.maxAttendances = this.attendances;
+                    if (this.studentName===this.currentStudent) this.currentAttendances= this.attendances;    
+                    this.chances.push(this.attendances);
+                    this.attendances=0;
                 }
 
                 this.attendanceArray.sort((a, b) => {
                     return (a.weekNumber > b.weekNumber);
                 });
+
+                this.currentAttendancies=this.currentAttendancies/this.maxAttendances;
+                for(let [index, chance] of this.chances.entries())
+                {
+                   this.chances[index]=this.chances[index]/this.maxAttendances; 
+                }
+                
                 this.$refresh();
 
             }
-
             this.lookuperr = () => {
             }
-            this.tableHeader = ["First name", "Last name", "Cnp", "Sansa Promovare"];
+
+
+            this.tableHeader = ["First name", "Last name", "Sansa Promovare"];
 
             this.recommendations = ["https://www.w3schools.com/", "google.com", "TudorSorin PaginiWeb"];
 
