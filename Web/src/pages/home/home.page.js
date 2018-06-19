08ca9c3cb30d755fd9aa2f392fd7a93d7b6d2b48
Mihaila Andrei <andyfbgm@gmail.com>
2018-06-20 01:35:30 +0300

    progress 0.1

--- Web/src/pages/home/home.page.js
+++ Web/src/pages/home/home.page.js
@@ -1,9 +1,47 @@
 var rt = require('../../../framework/router');
+var services = require('../../services/index');
+var codebird = require('../../../node_modules/codebird');
 (() => {
     rt.route('/', {
         templateUrl: './src/pages/home/home.page.html',
         styleUrl: './src/pages/home/home.page.css'
     },
-        function () {
-        });
+    function () {
+
+        this.role = '';
+        this.cb = new codebird;
+        
+        this.$onInit = () => {
+            this.role = services.AuthService.getUserRole();
+            this.cb.setConsumerKey('qNmVG4mMKW76TNI9pPhSRwt1h','HDqJqH47FVNnb8PATfaTHEQiqnmQcpWS77sVNZNNaJwBPF1nBE');
+        }
+
+        // ------------------------------------------------------------------
+        // TWITTER
+
+        this.$on('#twitterbutton','click',function () {
+            this.initTwitter();
+        }.bind(this));
+        
+        this.initTwitter = () => {
+            this.cb.__call(
+                "oauth_requestToken",
+                {oauth_callback: "oob"},
+                function (reply) {
+                    // stores it
+                    this.cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+             
+                    // gets the authorize screen URL
+                    this.cb.__call(
+                        "oauth_authorize",
+                        {},
+                        function (auth_url) {
+                            window.codebird_auth = window.open(auth_url);
+                        }
+                    );
+                }
+            );
+        }
+        // ------------------------------------------------------------------
+    });
 })();
